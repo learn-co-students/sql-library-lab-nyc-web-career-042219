64310594e076c1b0b5fb79ea6687da7cf20833c4
github username <github email address>
2019-05-01 15:00:34 -0400

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -48,16 +48,3 @@ def select_character_names_and_number_of_books_they_are_in
   GROUP BY character_books.character_id
   ORDER BY(COUNT(character_books.character_id)) DESC, characters.name;"
 end
-
-
-
-
-
-
-SELECT tweets.*
-FROM tweetsandtags
-JOIN tags
-ON tweetsandtags.tag_id = tags.id
-WHERE tag.name = "#tbt"
-JOIN tweets
-ON tweetsandtags.tweet_id = tweet.id
