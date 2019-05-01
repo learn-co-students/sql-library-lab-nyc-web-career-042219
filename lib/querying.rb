13e4ce6ae434ca3173a584da93660e9c6d595759
github username <github email address>
2019-05-01 14:57:39 -0400

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,8 +23,8 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
-  FROM series
-  JOIN authors
+  FROM authors
+  JOIN series
   ON authors.id = series.author_id
   JOIN subgenres
   ON subgenres.id = series.subgenre_id;"
@@ -48,3 +48,16 @@ def select_character_names_and_number_of_books_they_are_in
   GROUP BY character_books.character_id
   ORDER BY(COUNT(character_books.character_id)) DESC, characters.name;"
 end
+
+
+
+
+
+
+SELECT tweets.*
+FROM tweetsandtags
+JOIN tags
+ON tweetsandtags.tag_id = tags.id
+WHERE tag.name = "#tbt"
+JOIN tweets
+ON tweetsandtags.tweet_id = tweet.id
